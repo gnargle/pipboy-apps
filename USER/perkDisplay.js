3e3c3d2d8a12aefcb75db2dd8e9faab3cd900fb6
Athene Allen <atheneallen93@gmail.com>
2025-03-21 22:25:16 +0000

    center perk icons

--- USER/perkDisplay.js
+++ USER/perkDisplay.js
@@ -2,6 +2,8 @@ const perkListMax = 10;
 const fontSizeTitle = 16;
 const fontSizeDesc = 14;
 const titleOffsetY = 20;
+const perkImageXMaxSize = 167;
+const perkImageYMaxSize = 153;
 
 function draw(selected){
   bC.clear(); 
@@ -13,17 +15,15 @@ function draw(selected){
 
 function buildList(directory, selected){   
   let files = require("fs").readdirSync(directory)
-  log("filelist: " + files);
   let max = files.length >= files ? perkListMax : files.length;
   for(i = 0; i < max; i++){
     let file = files[i];
-    log("loading file:" + file);
     let fileString = require("fs").readFileSync( directory + "/" + file);
     let fileObj = JSON.parse(fileString);
     if (i == selected){
       drawPerk(fileObj);
       drawSelectedPerkOutline(i);
-    }
+    }  
     drawPerkTitle(fileObj.title, i);
   }  
 }
@@ -34,10 +34,17 @@ function drawPerk(fileObj){
 }
 
 function drawPerkImage(imgStr, xSize, ySize){
+  //so we want to try and draw as central as possible.
+  //our 'window' for how big the perk can be is 167x153 (y truncated for the desc at the bottom)
+  //to figure out how to centre the image we need to get the difference
+  //between the size of the image and our window, halve it, and add it on
+  //to the location.
+  let xOffset = Math.floor((perkImageXMaxSize - xSize)/2);
+  let yOffset = Math.floor((perkImageYMaxSize - ySize)/2);
   bC.drawImage({
     width : xSize, height : ySize, bpp : 1,
     buffer : require("heatshrink").decompress(atob(imgStr))
-  }, 200,0);  
+  }, 200 + xOffset,0 + yOffset);  
 }
 
 function drawPerkTitle(title, i){
