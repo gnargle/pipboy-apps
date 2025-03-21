7fcd1c2445dfdca703f0bc7ff2fc486dc1020bb5
Athene Allen <atheneallen93@gmail.com>
2025-03-21 19:50:17 +0000

    screen layout, title and desc display done

--- USER/perkDisplay.js
+++ USER/perkDisplay.js
@@ -1,14 +1,50 @@
-function showPerkImage(file){
-  let imgStr = require("fs").readFileSync(file);
-  bC.clear();
-  bC.drawImage({
-    width : 167, height : 167, bpp : 1,
-    buffer : require("heatshrink").decompress(atob(imgStr))
-  }, 50,50);
-  bC.flip();
+const perkListMax = 10;
+
+function buildList(directory, selected){
+  bC.clear();  
+  let files = require("fs").readdirSync(directory)
+  log("filelist: " + files);
+  let max = files.length >= files ? perkListMax : files.length;
+  for(i = 0; i < max; i++){
+    let file = files[i];
+    log("loading file:" + file);
+    let fileString = require("fs").readFileSync( directory + "/" + file);
+    let fileObj = JSON.parse(fileString);
+    if (i == selected){
+      drawPerk(fileObj);
+      drawSelectedPerkOutline(i);
+    }
+    drawPerkTitle(fileObj.title, i);
+  }
+  bH.flip();
+  bF.flip();
+  bC.flip();  
 }
 
-showPerkImage("USER/IMG/cherchez.b64");
-setTimeout(() => {
-  showPerkImage("USER/IMG/actiongirl.b64");
-}, 3000);
+function drawPerk(fileObj){  
+    drawPerkImage(fileObj.img, fileObj.xSize, fileObj.ySize);
+    drawPerkDesc(fileObj.description);
+}
+
+function drawPerkImage(imgStr, xSize, ySize){
+  bC.drawImage({
+    width : xSize, height : ySize, bpp : 1,
+    buffer : require("heatshrink").decompress(atob(imgStr))
+  }, 200,0);  
+}
+
+function drawPerkTitle(title, i){
+  bC.setFontVector(16);
+  bC.drawString(title, 10, (20 * i) + 5);
+}
+
+function drawPerkDesc(desc){
+  bC.setFontVector(14);
+  bC.drawString(desc, 10, 150);
+}
+
+function drawSelectedPerkOutline(i){
+  bC.drawRect(5,0,190,23)
+}
+
+buildList("USER/PERKS", 0);
