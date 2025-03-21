796c4f741df833bf054fcfe88dba4aecfef0cb8a
Athene Allen <atheneallen93@gmail.com>
2025-03-21 18:48:23 +0000

    display multiple perks in a row

--- USER/perkDisplay.js
+++ USER/perkDisplay.js
@@ -1,8 +1,14 @@
-let imgStr = require("fs").readFileSync("USER/IMG/cherchez.b64");
-log("img string contents: " + imgStr);
-bC.clear();
-bC.drawImage({
-  width : 167, height : 167, bpp : 1,
-  buffer : require("heatshrink").decompress(atob(imgStr))
-}, 50,50);
-bC.flip();
+function showPerkImage(file){
+  let imgStr = require("fs").readFileSync(file);
+  bC.clear();
+  bC.drawImage({
+    width : 167, height : 167, bpp : 1,
+    buffer : require("heatshrink").decompress(atob(imgStr))
+  }, 50,50);
+  bC.flip();
+}
+
+showPerkImage("USER/IMG/cherchez.b64");
+setTimeout(() => {
+  showPerkImage("USER/IMG/actiongirl.b64");
+}, 3000);
