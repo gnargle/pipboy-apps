b6bcbd96aa02553ed244a7a8c783cfea0f5602d8
Athene Allen <atheneallen93@gmail.com>
2025-03-24 17:22:31 +0000

    add the ability to modify stats on-device by clicking in the left wheel and turning it.

--- USER/statsDisplay.js
+++ USER/statsDisplay.js
@@ -10,6 +10,7 @@ const maxScreen = 2;
 const perkScreen = 2;
 const statScreen = 1;
 const SPECIALScreen=0;
+const perkSelectionScreen = 3;
 
 Graphics.prototype.setFontMonofonto14 = function() {
   // Actual height 14 (13 - 0)
@@ -32,6 +33,8 @@ function draw(){
     buildScreen("USER/SKILLS");
   } else if (screenSelected == SPECIALScreen){
     buildScreen("USER/SPECIAL");
+  } else if (screenSelected == perkSelectionScreen){
+    buildPerkSelectionScreen();
   }
   bH.flip();
   bF.flip();
@@ -40,7 +43,7 @@ function draw(){
 }
 
 function buildScreen(directory){
-    let files = require("fs").readdirSync(directory)
+    let files = require("fs").readdirSync(directory);
     loadedListMax = files.length;
     let entryListMax = Math.min(entryListDisplayMax, loadedListMax);
     let a = 0;
@@ -57,12 +60,20 @@ function buildScreen(directory){
       if (i == entrySelected){
         drawEntry(fileObj);
         drawSelectedEntryOutline(i%entryListDisplayMax);
+        if (!configMode && screenSelected != perkScreen){
+          //only update pointsOfSelected when not actively configuring.
+          pointsOfSelected = fileObj.points;
+        }
       }
       drawEntryTitle(fileObj.title, i%entryListDisplayMax, i==entrySelected);
       if (screenSelected != perkScreen){
         drawEntryPoints(fileObj.points, i%entryListDisplayMax, i==entrySelected); 
       }
     }
+}
+
+function buildPerkSelectionScreen(){
+
 }
 
 function drawEntry(perkObj){  
@@ -104,7 +115,13 @@ function drawEntryPoints(points, i, selected){
   else {
     bC.setColor(colourWhite);
   }
-  bC.drawString(points, 160, (titleOffsetY * i) + 5);  
+  if (configMode && selected){
+    bC.fillPoly([129,(titleOffsetY * i) + 12,140,(titleOffsetY * i) + 12,135,(titleOffsetY * i) + 5]);
+    bC.fillPoly([141,(titleOffsetY * i) + 12,151,(titleOffsetY * i) + 12,146,(titleOffsetY * i) + 17]);
+    bC.drawString(pointsOfSelected, 160, (titleOffsetY * i) + 5);  
+  } else {
+    bC.drawString(points, 160, (titleOffsetY * i) + 5);  
+  }
 }
 
 function drawEntryDesc(desc){
@@ -118,23 +135,85 @@ function drawSelectedEntryOutline(i){
   bC.fillRect(5,(titleOffsetY * i) + 1,190,(titleOffsetY * i) + 23)
 }
 
+function saveFile(directory){
+  let files = require("fs").readdirSync(directory);  
+  let fileToSave = directory + "/" + files[entrySelected];
+  let fileString = require("fs").readFileSync(fileToSave);
+  let fileObj = JSON.parse(fileString);
+  fileObj.points = pointsOfSelected;
+  fileString = JSON.stringify(fileObj);
+  require("fs").writeFile(fileToSave, fileString);
+}
+
+function saveNewValue(){
+  //so to save we need: 
+  // * the screen we're on and the folder we're using
+  // * the selected entry
+  // * the selected entries new points
+  if (screenSelected == SPECIALScreen){
+    saveFile("USER/SPECIAL");
+  } else if (screenSelected == statScreen){
+    saveFile("USER/SKILLS");
+  }
+}
+
+function handleKnob1Config(dir){
+  //first, play the click.
+  Pip.knob1Click(dir);
+
+  if (dir == 0){
+    //pressed in, this is the config trigger.    
+    //toggle configuration mode on special/skills screens
+    configMode = !configMode;
+    saveNewValue();
+    Pip.removeListener("knob1",handleKnob1Config); 
+    Pip.on("knob1",handleKnob1);   
+    registeredKnob1Func = handleKnob1;
+  } else {
+    pointsOfSelected += dir; 
+    if (pointsOfSelected < 0){
+      pointsOfSelected = 100;
+    } else if (pointsOfSelected > 100){
+      entrySelected = 0;
+    }
+  }
+  draw();
+}
+
 function handleKnob1(dir){
   //first, play the click.
   Pip.knob1Click(dir);
-  //then, we need to change our position in the list.
-  entrySelected -= dir; //-1 is scroll down, but our list increases numerically. so we need to subtract
-  if (entrySelected < 0){
-    entrySelected = loadedListMax - 1;
-  } else if (entrySelected >= loadedListMax){
-    entrySelected = 0;
+
+  if (dir == 0){
+    //pressed in, this is the config trigger.
+    if (screenSelected == perkScreen){
+      //change screen to perk selection screen.
+      screenSelected = perkSelectionScreen;
+    } else {
+      //toggle configuration mode on special/skills screens
+      configMode = !configMode;
+      Pip.removeListener("knob1",handleKnob1); 
+      Pip.on("knob1",handleKnob1Config);
+      registeredKnob1Func = handleKnob1Config;
+    }
+  } else {
+    //then, we need to change our position in the list.
+    entrySelected -= dir; //-1 is scroll down, but our list increases numerically. so we need to subtract
+    if (entrySelected < 0){
+      entrySelected = loadedListMax - 1;
+    } else if (entrySelected >= loadedListMax){
+      entrySelected = 0;
+    }
   }
+
   draw();
 }
 
 function handleKnob2(dir){
   //first, play the click.
-  Pip.knob2Click(dir);
-  log("knob2 direction: " + dir);
+  Pip.knob2Click(dir);  
+  //if switching screens we need to boot immediately out of config without saving.
+  configMode = false; 
   //then switch stats screen.
   screenSelected += dir;
   entrySelected = 0; //reset to top of list
@@ -155,7 +234,7 @@ function gracefulClose(){
   //shut down interval triggers first in case one fires while we're tearing down
   clearInterval(modeCheck);
   clearInterval(intervalId);
-  Pip.removeListener("knob1",handleKnob1); 
+  Pip.removeListener("knob1",registeredKnob1Func);
   Pip.removeListener("knob2",handleKnob2); 
   Pip.removeListener("torch",handleTorch); 
   showMainMenu(); //this causes a brief flicker but if we don't do it the controls stop working.
@@ -168,14 +247,24 @@ function ourModeHandler(){
   }
 }
 
+function powerHandler(){
+  gracefulClose();
+  //may need to also trigger the default power handler here also depending on if this overwrites the other one,
+  //that is Pip.powerButtonHandler();
+}
+
 let loadedListMax = 0;
 let entrySelected = 0;
 let screenSelected = 0;
+let pointsOfSelected = 0;
 let drawing=false;
+let configMode = false;
 
 Pip.on("knob1",handleKnob1);
+let registeredKnob1Func = handleKnob1;
 Pip.on("knob2",handleKnob2);
 Pip.on("torch",handleTorch);
+setWatch(powerHandler,BTN_POWER,{repeat:false})
 draw();
 let modeCheck = setInterval(ourModeHandler,100);
 let intervalId = setInterval(() => {  
