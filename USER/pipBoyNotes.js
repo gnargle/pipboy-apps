3a2053c6f066f135dbce39d82e4ba9d109210423
Athene Allen <atheneallen93@gmail.com>
2025-10-23 17:51:58 +0100

    comment

--- USER/pipBoyNotes.js
+++ USER/pipBoyNotes.js
@@ -4,6 +4,8 @@ try{
     pbkb.assembledString = fs.readFileSync("USER/note.txt");
 }catch{}
 
+//main loop of the keyboard library. This draws to the whole screen and clears all
+//input on knob1 - so make sure not to draw or attach to knob1 before pbkb.finished == true!
 pbkb.textEntryLoop();
 
 let intervalId = setInterval(() => {
